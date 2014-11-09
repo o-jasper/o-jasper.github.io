9d924e9bc7b4530b1ce120f02b57c5077b301875
Jasper den Ouden <o.jasper@gmail.com>
2014-11-10 00:46:35 +0100

    Added construct from types and values

--- ethereum-js-explore/js/deconstruct.js
+++ ethereum-js-explore/js/deconstruct.js
@@ -19,11 +19,35 @@ function deconstruct(hex, types) {
             out.push(eth.toAscii(curhex));
         } else if( type == "uint" ) { //Unsigned integer.
             out.push(parseInt(curhex));
-        }
+        } else{ alert("unidentified type"); }
         i ++;
         n += type[1];
     }
     out.push("0x" + hex.substr(n));
+    return out;
+}
+
+function construct(values, types) {
+    if( values.length != types.length ){ alert("Constructing hex value mismatch"); }
+    var out = "0x";
+    for(var i in values) {
+        var tp = types[i][0], len=types[i][1];
+        if(tp == "raw") {
+            if( values[i].subset(0,2)!="0x" ){ alert("this isnt hex"); }
+            if( values[i].length > len ){ alert("hex string too long"); }
+            out += values[i];
+            for( var k=0 ; k < len - values.length ; k++){ out += "0"; }
+        } else if(tp == "ascii") {
+            var got_str = eth.fromAscii(types[i], len).substr(2);
+            if( got_str.length != len ){ alert("wrong len"); }
+            out += got_str;
+        } else if(tp == "uint" ) {
+            var got_str = values[i].toString(16);
+            if( got_str.length > len ){ alert("too large unsigned"); }
+            for( var k=0 ; k < len - got_str.length ; k++){ got_str += "0"; }
+            out += got_str;
+        } else{ alert("unidentified type"); }
+    }
     return out;
 }
 
@@ -35,6 +59,8 @@ function new_typedict(name_len) {
         lengths:{},
         name_len:name_len, // Lengthen names to this.
 
+        name_hex:function(name_str){ return eth.fromAscii(name_str, this.name_len); }
+        
         add:function(name, types) {
             this.iface[name] = types;
             var cnt = this.lengths[name.length];
@@ -42,9 +68,7 @@ function new_typedict(name_len) {
             else{ this.lengths[name.length] = cnt + 1; }
         }
 
-        add_str:function(name_str, types) {
-            this.add(eth.fromAscii(name_str, this.name_len), types);
-        }
+        add_str:function(name_str, types) { this.add(this.name_hex(name_str), types); }
         
         remove:function(name) {
             var cnt = this.lengths[name.length];
@@ -63,6 +87,13 @@ function new_typedict(name_len) {
                 }
             }
             // None of the definitions match.
+        }
+        construct:function(name, values) {
+            var types = iface[name];
+            if( types != undefined ){ return contruct(values, types); }
+        }
+        construct_str:function(name_str, values) {
+            return this.construct(this.name_hex(name_str), values);
         }
     }
 }
--- ethereum-js-explore/readme.md
+++ ethereum-js-explore/readme.md
@@ -4,4 +4,7 @@ Stuff to show messages, currently, but other javascript visualizing stuff.
 There is also a 'message data deconstructor', that takes a list of data types.
 TODO is to add converting forward too, bijection test, and try it.
 
+Would be good to have a database of APIs and recognizing which contracts use
+which APIs. Not sure how to do that at this point.
+
 Licensed under GPLv3.
