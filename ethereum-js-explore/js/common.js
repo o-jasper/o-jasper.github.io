9b747eec82f9bec3e0c77a64d2bc8d8a8c9bd9e9
Jasper den Ouden <o.jasper@gmail.com>
2014-11-10 00:09:55 +0100

    Add license

--- ethereum-js-explore/js/common.js
+++ ethereum-js-explore/js/common.js
@@ -1,3 +1,9 @@
+//  Copyright (C) 10-11-2014 Jasper den Ouden.
+//
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
 // Get element.
 function ge(element_id) { return document.getElementById(element_id); }
