a3f29a853471154f661d85fc336e77c10a2de6bc
Jasper den Ouden <o.jasper@gmail.com>
2014-12-05 01:56:29 +0100

    stuff

--- eth-entity/assurance_entity/js/contract.js
+++ eth-entity/assurance_entity/js/contract.js
@@ -2,16 +2,26 @@
 function new_crowdfund(_addr) {
     return function(){
         addr : _addr,
-        balance   : function(){ return eth.block.balanceOf(addr); }
-        creator   : function(){ return eth.block.stateAt(addr, "0x00"); }
-        recipient : function(){ return eth.block.stateAt(addr, "0x20"); }
-        endblock  : function(){ return parseInt(eth.block.stateAt(addr, "0x40")); }
-        min       : function(){ return parseInt(eth.block.stateAt(addr, "0x60")); }
-        max       : function(){ return parseInt(eth.block.stateAt(addr, "0xA0")); }
-        cnt       : function(){ return parseInt(eth.block.stateAt(addr, "0x80")); }
+        // Accessing stuff.
+        balance   : function() { return eth.block.balanceOf(addr); }
+        creator   : function() { return eth.block.stateAt(addr, "0x00"); }
+        recipient : function() { return eth.block.stateAt(addr, "0x20"); }
+        endblock  : function() { return parseInt(eth.block.stateAt(addr, "0x40")); }
+        min       : function() { return parseInt(eth.block.stateAt(addr, "0x60")); }
+        max       : function() { return parseInt(eth.block.stateAt(addr, "0xA0")); }
+        cnt       : function() { return parseInt(eth.block.stateAt(addr, "0x80")); }
 
-        reached   : function(){
+        reached   : function() {
             return this.balance() > this.min()
+        }
+        // Doing stuff.
+        do_create : function(creator, recipient, endtime) {
+            var priv = got_privkey(creator);
+            if( priv == null ){ alert("Do not have private key to that"); return; }
+            if( creator != recipient ){
+                alert("currently can only have creator and recipient identical.");
+            }
+            build_create_crowdfund(priv);
         }
     }
 }
--- eth-entity/assurance_entity/js/gui.js
+++ eth-entity/assurance_entity/js/gui.js
@@ -12,46 +12,6 @@
 // * Putting coin in.
 //
 
-els = { // Elements.
-    els : {},
-    
-    ge : function(id) {
-        if( this.els[id] == undefined ){ // Setting `els` manually overrides.
-            this.els[id] = document.getElementById(id);
-        }
-        return this.els[id];
-    } 
-
-    addr_note : function(id, value = null, must_own=null) {
-        if( value == null ){ value = hexify(els.ge(id).value); }
-        var note = ge(id + "_note");            
-        if( !is_addr(value) ){
-            note.innerText = "Not an address"; note.className = "invalid";
-            return null;
-        }
-        if(must_own && got_privkey(value)==null) {
-            note.innerText = "Dont have privkey"; note.className = "warn";
-            if(must_own == "validity") {
-                return null; 
-            } else {
-                return value;
-            }
-        }
-        note.innerText = ""; note.className = "";
-        return value;
-    }
-
-    int_note : function(id) {
-        var string = els.ge(id).value;
-        if( !is_int(string) ){ 
-            var note = ge(id + "_note");
-            note.innerText = "Not an integer"; note.className = "invalid";
-            return null;
-        }
-        return parseInt(string);
-    }
-}
-
 function new_crowdfund_gui()  {
     return {
         crowdfund : null,
